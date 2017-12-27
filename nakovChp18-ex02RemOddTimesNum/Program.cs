cf49949e69f27d8e15ed91ed5cb5c9ac9bc303c8
vancisin peter <vancisin.peter@gmail.com>
2017-12-27 20:32:16 +0100

    Add project files.

---
+++ nakovChp18-ex02RemOddTimesNum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nakovChp18_ex02RemOddTimesNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
