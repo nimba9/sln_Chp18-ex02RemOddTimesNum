2a91cdd5cd6beb467352e93102de10f453285921
vancisin peter <vancisin.peter@gmail.com>
2017-12-27 22:16:15 +0100

    1st iteration

--- nakovChp18-ex02RemOddTimesNum/Program.cs
+++ nakovChp18-ex02RemOddTimesNum/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace nakovChp18_ex02RemOddTimesNum
+namespace RemoveNumbers
 {
-    class Program
+    public class DelOddTimesAppearingNum
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string inputSeq = Console.ReadLine();
+            string[] inpNumStrArr = inputSeq.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<int, int> numSeqDict = new Dictionary<int, int>();
+
+            List<int> numList = new List<int>();
+
+            for (int i = 0; i < inpNumStrArr.Length; i++)
+            {
+                int currNum = Convert.ToInt32(inpNumStrArr[i]);
+                numList.Add(currNum);
+                if (!numSeqDict.ContainsKey(currNum))
+                {
+                    numSeqDict.Add(currNum, 0);
+                }
+                else
+                {
+                    numSeqDict[currNum]++;
+                }
+            }       
+
+            StringBuilder filter = new StringBuilder();
+            for (int i = 0; i < numList.Count; i++)
+            {
+                if (numSeqDict[numList[i]] % 2 != 0)
+                {
+                    filter.AppendFormat("{0} ", numList[i]);  
+                }
+            }
+
+            if (filter.Length != 0)
+            {
+                Console.WriteLine(filter.ToString().TrimEnd());
+            }
         }
     }
 }
